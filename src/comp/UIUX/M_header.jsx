0cd7926d22461e526f46187e37ac9c741e67acf7
dnaals <swm8793@naver.com>
2024-08-26 21:09:45 +0900

    버거메뉴 완성

--- src/comp/UIUX/M_header.jsx
+++ src/comp/UIUX/M_header.jsx
@@ -1,12 +1,20 @@
+"use client";
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import '../style/header.scss';
 function M_header(props) {
+
+    let [menuOn , setMenuOn] = useState(false);
+    const menuClick = ()=>{
+        setMenuOn(!menuOn)
+        console.log(menuOn)
+    }
+
     return (
-        <header>
+        <header className={menuOn ? 'on':''}>
             <h1 className='Logo'><Link href='/'><Image src='/images/logo.png' alt='logo' width={153} height={30}/>Mov_logo</Link></h1>
-            <div className="menu_btn"><span>menu</span></div>
+            <div className="menu_btn" onClick={menuClick}><span>menu</span></div>
             <div className='menu'>
                 <nav>
                     <form >
@@ -18,7 +26,24 @@ function M_header(props) {
                     <Link href="/mypage">My page</Link>
                 </nav>
             </div>
-            
+            <div className={menuOn ? 'sub_menu on':'sub_menu'}>
+                <form>
+                    <input type="text" placeholder='검색어를 입력하세요'/>
+                    <Image src='/images/search_icon.png' alt='search' width={18} height={18}/>
+                </form>
+                <div className='search_record'>
+                    <p># 아바타</p>
+                    <p># 조정석</p>
+                    <p># 마블</p>
+                    <p># 송우민</p>
+                    <p># 지연</p>
+                </div>
+                <div className='linked'>
+                    <Link href="/reserve">Ticket</Link>
+                    <Link href="/login">Login</Link>
+                    <Link href="/mypage">My page</Link>
+                </div>
+            </div>
         </header>
     );
 }
