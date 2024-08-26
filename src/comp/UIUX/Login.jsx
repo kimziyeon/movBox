bd9a0842c66ec3778078abbb57bb2eac22449289
dnaals <swm8793@naver.com>
2024-08-26 21:53:07 +0900

    로그인 html틀 잡기

--- src/comp/UIUX/Login.jsx
+++ src/comp/UIUX/Login.jsx
@@ -2,8 +2,17 @@ import React from 'react';
 import '../style/login.scss'
 function Login(props) {
     return (
-        <div>
-            로그인페이지
+        <div className='login'>
+            <h2>로그인</h2>
+            <form className='login_space'>
+                <input type="text" placeholder='아이디'/>
+                <input type="text" placeholder='비밀번호' />
+                <button>로그인</button>
+            </form>
+            <div className='personal_info'>
+                <p>아이디 / 비밀번호 찾기</p>
+                <p>회원가입</p>
+            </div>
         </div>
     );
 }
