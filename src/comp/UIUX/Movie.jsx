a6b029f612dedf531530757280479b48dada3668
kimziyeon <aa40254037@gmail.com>
2024-08-27 16:13:33 +0900

    메인 작업중

--- src/comp/UIUX/Movie.jsx
+++ src/comp/UIUX/Movie.jsx
@@ -8,6 +8,7 @@ import { useStore } from '../store/movie_store';
 import { useStore4 } from '../store/movie_poster';
 import { movie_server } from '../store/movie_server';
 import { format, subDays } from 'date-fns';
+import Image from 'next/image';
 
 function Movie(props) {
     let { dataFetch, dailyBoxOffice, dailyRank, movieCode, movieDate } = useStore();
@@ -36,19 +37,34 @@ function Movie(props) {
             <section className='main_movie'>
                 <ul>
                     <li>
-                        <h2 className='poster'>영화포스터</h2>
-                        <b>영화이름</b>
-                        <div className='reserve_rate'>
-                            <h4>예매율</h4>
-                            <p>1<span>st</span></p>
+                        <div className="poster">
+                            <Image src="/images/아바타.jpg" layout="responsive"
+                                width={100}
+                                height={200}
+                            />
+                            <div className="overlay"></div>
                         </div>
-                        <div className='grade'>
-                            <h4>평점</h4>
-                            <p>9.3</p>
-                        </div>
-                        <div className="audience">
-                            <h4>누적관객(만)</h4>
-                            <p>50<span>+</span></p>
+
+                        <div className="txt_cont">
+                            <h3 className="name">아바타: 물의 길</h3>
+                            <h3 className="sub_name">Avatar: The Way of Water</h3>
+
+                            <div className='reserve_rate'>
+                                <h4>예매율</h4>
+                                <p>1<span>st</span></p>
+                            </div>
+                            <div className='grade'>
+                                <h4>평점</h4>
+                                <div class="material-icons">
+                                    star
+                                </div>
+                                <p>9.3</p>
+                            </div>
+                            <div className="audience">
+                                <h4>누적관객(만)</h4>
+                                <p>50<span>+</span></p>
+                            </div>
+
                         </div>
                     </li>
                 </ul>
