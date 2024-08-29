cead318628fd9ebf08a0bb4a14cf4776f6215da6
kimziyeon <aa40254037@gmail.com>
2024-08-29 16:02:29 +0900

    메인스와이퍼

--- src/comp/UIUX/Movie.jsx
+++ src/comp/UIUX/Movie.jsx
@@ -39,63 +39,109 @@ function Movie(props) {
         <>
             <article className='main_movie'>
                 <Swiper
+
                     spaceBetween={30}
                     centeredSlides={true}
                     // autoplay={{
-                    //     delay: 2500,
+                    //     delay: 5000,
                     //     disableOnInteraction: false,
                     // }}
 
                     modules={[Autoplay]}
                     className="mySwiper"
+                    style={{ height: 'auto' }}
                 >
-                    <SwiperSlide>
+                    <SwiperSlide >
                         <div className="poster">
-                            <Image src="/images/아바타.jpg" width={1000} height={1500} />
+                            <Image src="/images/아바타.jpg"
+                                width={1000} height={1500}
+                                alt="아바타 포스터"
+                                priority />
                             <div className="overlay"></div>
                         </div>
 
-                    </SwiperSlide>
-
-                    <SwiperSlide>
-                        <div className="poster">
-                            <Image src="/images/아바타.jpg" width={1000} height={1500} />
-                            <div className="overlay"></div>
+                        <div className="poster_pc">
+                            <Image src="/images/아바타.jpg"
+                                width={1000} height={1500}
+                                alt="아바타 포스터"
+                            />
                         </div>
-
 
                         <div className="inner">
-                            <div className="txt_cont">
+                            <div className="txt_group">
                                 <h3 className="name">아바타: 물의 길</h3>
                                 <h3 className="sub_name">Avatar: The Way of Water</h3>
 
-                                <div className='reserve_rate'>
-                                    <h4>예매율</h4>
-                                    <p>1<span>st</span></p>
-                                </div>
-                                <div className='grade'>
-                                    <h4>평점</h4>
-                                    <Image src="/images/star_icon.svg" width={15} height={15} className="star_icon" />
-                                    <p>9.3</p>
-                                </div>
-                                <div className="audience">
-                                    <h4>누적관객(만)</h4>
-                                    <Image src="/images/person_icon.svg" width={15} height={15} className="person_icon" />
-                                    <p>547,000+<span>+</span></p>
+                                <div className="txt_conts">
+                                    <div className='reserve_rate'>
+                                        <h4>예매율</h4>
+                                        <p>1<span> st</span></p>
+                                    </div>
+                                    <div className='grade'>
+                                        <h4>평점</h4>
+                                        <Image src="/images/star_icon.svg" width={15} height={15} className="star_icon" />
+                                        <p>9.3</p>
+                                    </div>
+                                    <div className="audience">
+                                        <h4>누적관객(만)</h4>
+                                        <Image src="/images/person_icon.svg" width={15} height={15} className="person_icon" />
+                                        <p>547,000<span> +</span></p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
 
                     </SwiperSlide>
+
+                    <SwiperSlide>
+                        <div className="poster">
+                            <Image src="/images/아바타.jpg" width={1000} height={1500} />
+                            <div className="overlay"></div>
+                        </div>
+
+                        <div className="poster_pc">
+                            <Image src="/images/아바타.jpg"
+                                width={1000} height={1500}
+                                alt="아바타 포스터"
+                            />
+                        </div>
+
+                        <div className="inner">
+                            <div className="txt_group">
+                                <h3 className="name">아바타: 물의 길</h3>
+                                <h3 className="sub_name">Avatar: The Way of Water</h3>
+
+                                <div className="txt_conts">
+                                    <div className='reserve_rate'>
+                                        <h4>예매율</h4>
+                                        <p>1<span> st</span></p>
+                                    </div>
+                                    <div className='grade'>
+                                        <h4>평점</h4>
+                                        <Image src="/images/star_icon.svg" width={15} height={15} className="star_icon" />
+                                        <p>9.3</p>
+                                    </div>
+                                    <div className="audience">
+                                        <h4>누적관객(만)</h4>
+                                        <Image src="/images/person_icon.svg" width={15} height={15} className="person_icon" />
+                                        <p>547,000<span> +</span></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </SwiperSlide>
                 </Swiper>
 
-                {/* 스와이퍼 넣을거면 ul li로 짤거고 안넣을거면 ul li 뺄예정 */}
 
             </article>
             <article className='box_office'>
                 <div className="inner">
                     <h2>박스 오피스</h2>
                     <ul>
+                        {/* 스와이퍼 넣을거면 ul li로 짤거고 안넣을거면 ul li 뺄예정 */}
+
                         {/* {
                         posterUrl.map((obj,k)=>(
                             <li key={k}><img src={obj} alt="poster" /></li>
