b02e4dd593168b10541dd05be44d5b096199df81
dnaals <swm8793@naver.com>
2024-08-31 21:33:41 +0900

    회원가입 유효성검사 완성

--- src/comp/UIUX/Join.jsx
+++ src/comp/UIUX/Join.jsx
@@ -1,27 +1,100 @@
-import React from 'react';
+"use client";
+import { useEffect, useState } from 'react';
 import '../style/join.scss'
-
-const fields = [
-    { id: 'id', label: '아이디', placeholder: '필수 | 2자 이상, 6자 이내', hasButton: true },
-    { id: 'pw', label: '비밀번호', placeholder: '필수 | 2자 이상' },
-    { id: 'name', label: '이름', placeholder: '필수 | 2자 이상' },
-    { id: 'email', label: '이메일', placeholder: '필수 | abc@mail.com', hasButton: true },
-    { id: 'birth', label: '생년월일', placeholder: '선택 | 19960105' },
-];
+import Image from 'next/image';
+import Link from 'next/link';
 
 function Join() {
+    const fields = [
+        { id: 'id', label: '아이디', placeholder: '필수 | 2자 이상, 6자 이내', hasButton: true },
+        { id: 'pw', label: '비밀번호', placeholder: '필수 | 2자 이상' },
+        { id: 'user_name', label: '이름', placeholder: '필수 | 2자 이상' },
+        { id: 'user_email', label: '이메일', placeholder: '필수 | abc@mail.com', hasButton: true },
+        { id: 'birth', label: '생년월일', placeholder: '선택 | 19960105' },
+    ];
+    
+    const ERROR_TABLE ={
+        id : '아이디는 2~6글자로 해주세요',
+        pw : '비빌번호는 6~20자 이내로 해주세요',
+        user_name : '이름을 2글자 이상으로 해주세요',
+        user_email : '이메일 형식을 확인해주세요',
+    }
+
+    let [inputValue,setInputValue] = useState({
+        id: '',
+        pw: '',
+        user_name: '',
+        user_email: '',
+        birth:''
+    });
+    const input_value = (e)=>{
+        const { className, value } = e.target;
+        setInputValue({
+            ...inputValue,
+            [className]: value
+        });
+    }
+
+
+    let [successJoin,setSuccessJoin] = useState(false);
+    let [valueCheck,setValueCheck] = useState({
+        id : true,
+        pw : true,
+        user_name : true,
+        user_email : true,
+        birth : true
+    })
+    const join = ()=>{
+        const checked = {
+            id : inputValue.id.length>=2 && inputValue.id.length<=6,
+            pw : inputValue.pw.length>=6 && inputValue.pw.length<=20,
+            user_name : inputValue.user_name.length>=2,
+            user_email :  /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/.test(inputValue.user_email),
+            birth : true
+        }
+        setValueCheck(checked);
+        if(Object.values(checked).every(obj=>obj)){
+            init();
+            console.log(inputValue);
+            setSuccessJoin(true);
+        } else{
+            console.log('실패')
+        }
+        function init(){
+            setInputValue({
+                id: '',
+                pw: '',
+                user_name: '',
+                user_email: '',
+                birth:''
+            })
+        }
+    }
+
+
+
+
     return (
-        <div className='join_membership'>
+        <>
+        <div className={successJoin?'join_membership success':'join_membership'}>
             <h2>회원가입</h2>
             {fields.map((field) => (
                 <div key={field.id} className={`check ${field.id}`}>
                     <p>{field.label}</p>
                     {field.hasButton && <button className='btn'>중복확인</button>}
-                    <input type="text" placeholder={field.placeholder} />
+                    <input type="text" placeholder={field.placeholder} onChange={input_value} className={field.id} value={inputValue[field.id]}/>
+                    <p className={valueCheck[field.id]? 'error':'error on'}>{ERROR_TABLE[field.id]}</p>
                 </div>
             ))}
-            <button className='btn'>회원가입</button>
+            <button className='btn' onClick={join}>회원가입</button>
         </div>
+        <div className={successJoin?'complete':'complete no'}>
+            <h2>회원가입 완료</h2>
+            <Image src='/images/mob_icon.png' width={60} height={52} alt='mob_icon'/>
+            <p>모브박스 회원이 되신것을 환영합니다.</p>
+            <Link href='/login'><button className='btn'>로그인</button></Link>
+        </div>
+        </>
     );
 }
 
