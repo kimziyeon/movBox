b3a136fb64f9575a5ca88795ad194791f64050f5
kimziyeon <aa40254037@gmail.com>
2024-09-03 18:12:01 +0900

    20240903 디테일페이지 css작업완료

--- src/app/detail/page.jsx
+++ src/app/detail/page.jsx
@@ -5,9 +5,9 @@ import React from 'react';
 function page(props) {
 
     return (
-        <>
+        <main>
             <Detail />
-        </>
+        </main>
     );
 }
 
--- src/comp/UIUX/Detail.jsx
+++ src/comp/UIUX/Detail.jsx
@@ -1,15 +1,13 @@
 //상세페이지
 
 "use client";
+import "../style/detail.scss";
 import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
-import "../style/detail.scss";
 import Link from 'next/link';
 import Image from 'next/image';
 
 function Detail(props) {
-
-
     return (
         <>
             <article className='detail_movie'>
@@ -24,52 +22,45 @@ function Detail(props) {
                     <div className="overlay"></div>
                 </div>
 
+                <div className="poster_pc">
+                    <Image src="/images/아바타.jpg"
+                        width={1000} height={1500}
+                        alt="아바타포스터"
+                    />
+                </div>
+
                 <div className="inner">
                     <div className="txt_group">
                         <h3 className="name">아바타: 물의 길</h3>
                         <h3 className="sub_name">Avatar: The Way of Water</h3>
-
-                        <div className="txt_conts">
-                            <div className='reserve_rate'>
-                                <h4>예매율</h4>
-                                <p>1</p>
-                            </div>
-                            <div className='grade'>
-                                <h4>평점</h4>
-                                <Image src="/images/star_icon.svg" width={15} height={15} className="star_icon" alt='star_icon' />
-                                <p>9.3</p>
-                            </div>
-                            <div className="audience">
-                                <h4>누적관객(만)</h4>
-                                <Image src="/images/person_icon.svg" width={15} height={15} className="person_icon" alt='person_icon' />
-                                <p>547,000<span> +</span></p>
-                            </div>
-                        </div>
                     </div>
 
                     <div className="txt_group_detail">
                         <div className='txt_conts_01'>
+                            <p className="genres">
+                                <span>액션</span>
+                                <span>어드벤쳐</span>
+                                <span>SF</span>
+                                <span>스릴러</span>
+                            </p>
+                        </div>
+                        <div className='txt_conts_02'>
                             <p><span>2022.12.14</span> 개봉</p>
                             <p><span>192</span> 분</p>
                             <p><span>12세</span> 이상</p>
                         </div>
-                        <div className='txt_conts_02'>
-                            <div className='txt_box_01'>
-                                <p>출연</p>
-                                <p>감독</p>
-                                <p>제작사</p>
-                            </div>
-                            <div className='txt_box_02'>
-                                <p>샐다나, 샘 워싱턴,  스티븐 랭, 케미트 윈슬렛, 시고니 위버</p>
-                                <p>제임스 카메론</p>
-                                <p>월트디즈니컴퍼니코리마 유한책임회사</p>
-                            </div>
-
-
+                        <div className='txt_conts_03'>
+                            <p className='txt_1'>출연</p>
+                            <p className='txt_2'>샐다나, 샘 워싱턴,  스티븐 랭, 케미트 윈슬렛, 시고니 위버</p>
+                            <p className='txt_3'>감독</p>
+                            <p className='txt_4'>제임스 카메론</p>
+                            <p className='txt_5'>제작사</p>
+                            <p className='txt_6'>월트디즈니컴퍼니코리마 유한책임회사</p>
                         </div>
+
                     </div>
 
-                    <Link href='reserve'><div className='btn mob'>예매하기</div></Link>
+                    <Link href='reserve'><div className='btn on mob'>예매하기</div></Link>
 
                 </div>
 
