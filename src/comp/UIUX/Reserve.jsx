8465d9d99e16c939696214e583442294d05605e1
kimziyeon <aa40254037@gmail.com>
2024-09-20 18:11:21 +0900

    20240920 예매페이지 유저선택완료

--- src/comp/UIUX/Reserve.jsx
+++ src/comp/UIUX/Reserve.jsx
@@ -5,71 +5,191 @@ import React, { useState, useEffect } from 'react';
 import { useStore } from '../store/movie_store';
 import { useSearchParams } from "next/navigation";
 import "../style/reserve.scss";
-import Link from 'next/link';
-import Image from 'next/image';
-import { format, subDays } from 'date-fns'; //여기추가
+import { format, subDays } from 'date-fns';
 
-function Reserve(props) {
-    let { dataFetch, dailyBoxOffice, movieCode } = useStore(); // 박스오피스 영화 10개 가져오는 스토어
+function Reserve({ props }) {
+    let { dataFetch, dailyBoxOffice, movieCode } = useStore();
     const params = useSearchParams()
-    const UsermovieCode = params.get('movieCd');
-
-    console.log(UsermovieCode, 'mmmmmmmmmmmovieCode')
-    // console.log(dailyBoxOffice, movieCode)
+    const usermovieCode = params.get('movieCd');
+    const [mvElIndex, setMvElIndex] = useState();
+    const [mvElTitle, setMvElTitle] = useState();
+    const [dateEl, setDateEl] = useState();
+    const [timeEl, setTimeEl] = useState();
+    const [isAllSelect, setIsAllSelect] = useState(null);
+    const [ingStep, setIngStep] = useState('movie')
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+    const [slideClass, setSlideClass] = useState('');
 
     const ticketMvList = dailyBoxOffice.map((mvTitle, id) => {
         return { mvTitle, mvCode: movieCode[id] };
     })
-    // console.log(ticketMvList)
 
-    const userMvSelect = (k, mvTitle) => {
-        console.log(k, mvTitle)
-    }
-    let fnsDate = new Date(); //여기추가
-    const today = format(fnsDate,'yyyy년MM월dd일')
-    
-    let yesterday = format(subDays(fnsDate, 1), "yyyyMMdd") //여기추가
     useEffect(() => {
-        dataFetch(yesterday) //여기추가
+        const handleResize = () => {
+            setScreenWidth(window.innerWidth);
+            if (window.innerWidth > 1024) {
+                setSlideClass('')
+            }
+        };
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+
+    //유저영화선택
+    const userMvSelect = (k, mvTitle) => {
+        setMvElIndex(k)
+        setMvElTitle(mvTitle)
+    }
+
+    //유저날짜선택
+    const userDateSelect = (date) => {
+        setDateEl(date)
+    }
+
+    const mvTimeList = [
+        '13 : 50',
+        '15 : 50',
+        '18 : 30',
+        '20 : 50',
+        '21 : 50'
+    ]
+
+    //유저시간선택
+    const userTimeSelect = (time, kk) => {
+        setTimeEl(time)
+    }
+
+    //선택완료버튼
+    const selectOK = () => {
+        if (screenWidth > 1024) { //1024이상 한번에 선택
+            if (!mvElTitle) {
+                alert('영화를 선택해주세요');
+                return;
+            }
+            if (!dateEl) {
+                alert('상영 날짜를 선택해주세요');
+                return;
+            }
+            if (!timeEl) {
+                alert('상영 시간을 선택해주세요');
+                return;
+            }
+            setIsAllSelect({
+                title: mvElTitle,
+                date: dateEl,
+                time: timeEl,
+            });
+            console.log(mvElTitle, dateEl, timeEl, '유저선택완료');
+        }
+        else if (ingStep === 'movie') {
+            if (!mvElTitle) {
+                alert('영화를 선택해주세요')
+                return;
+            }
+            setIngStep('date')
+            if (screenWidth >= 768 && screenWidth <= 1024) {
+                setSlideClass('slide_date')
+            }
+        } else if (ingStep === 'date') {
+            if (!dateEl) {
+                alert('상영 날짜를 선택해주세요')
+                return;
+            }
+            setIngStep('time')
+            if (screenWidth >= 768 && screenWidth <= 1024) {
+                setSlideClass('slide_time')
+            }
+        } else if (ingStep === 'time') {
+            if (!timeEl) {
+                alert('상영 시간을 선택해주세요');
+                return;
+            }
+            setIsAllSelect({
+                title: mvElTitle,
+                date: dateEl,
+                time: timeEl,
+            });
+            console.log(mvElTitle, dateEl, timeEl, '유저선택완료');
+        }
+
+    }
+
+    let fnsDate = new Date();
+    const today = format(fnsDate, 'yyyy년 MM월 dd일')
+
+    let tomorrDate = new Date(fnsDate);
+    tomorrDate.setDate(fnsDate.getDate() + 1)
+    const tomorrow = format(tomorrDate, 'yyyy년 MM월 dd일')
+
+    let yesterday = format(subDays(fnsDate, 1), "yyyyMMdd")
+    useEffect(() => {
+        dataFetch(yesterday)
     }, [])
 
-    //날짜는 내가 받아왔구 영화이름이나 이런건 userMvSelect 함수에서 가져온 mvTitle이용해서 영화제목 가져오면 될듯합니다?
+
+    useEffect(() => {
+        if (usermovieCode) {
+            const userFindMv = ticketMvList.findIndex(obj => obj.mvCode === usermovieCode);
+            setMvElIndex(userFindMv)
+            setMvElTitle(ticketMvList[userFindMv].mvTitle)
+        }
+    }, [usermovieCode])
 
     return (
         <>
             <article className='reserve'>
                 <div className='inner'>
                     <div className='user_selected'>
-                        <div className='user_mv'>아바타: 물의 길</div>
-                        <div className='user_date'>{today}</div>
-                        <div className='user_time'>20 : 50 ~</div>
+                        <div className='user_mv'>{mvElTitle}</div>
+                        <div className={`user_date ${dateEl ? 'border' : ''}`}>{dateEl}</div>
+                        <div className='user_time'>{timeEl}</div>
                     </div>
 
-                    <div className='list_box'>
-                        <div className='list_mv'>
+                    <div className={`list_box ${slideClass}`}>
+                        <div className={`list_mv ${ingStep === 'movie' ? 'block' : 'hidden'}`}>
                             {
                                 ticketMvList.map((obj, k) => (
-                                    <p key={k} onClick={() => { userMvSelect(k, obj.mvTitle) }}>
+                                    <p key={k}
+                                        className={`mvEl ${mvElIndex === k ? 'elSelect' : ''}`}
+                                        onClick={() => { userMvSelect(k, obj.mvTitle) }}
+                                    >
                                         {obj.mvTitle}
                                     </p>
                                 ))
                             }
 
                         </div>
-                        <div className='list_date'>
-                            <p>08월 14일</p>
-                            <p>08월 15일</p>
+                        <div className={`list_date ${ingStep === 'date' ? 'block' : 'hidden'}`}>
+                            <p className={`dateEl ${dateEl === today ? 'elSelect' : ''}`}
+                                onClick={() => { userDateSelect(today) }}
+                            >
+                                {today}
+                            </p>
+
+                            <p className={`dateEl ${dateEl === tomorrow ? 'elSelect' : ''}`}
+                                onClick={() => { userDateSelect(tomorrow) }}
+                            >
+                                {tomorrow}
+                            </p>
                         </div>
-                        <div className='list_time'>
-                            <p>13 : 50</p>
-                            <p>15 : 50</p>
-                            <p>18 : 30</p>
-                            <p>20 : 50</p>
-                            <p>21 : 50</p>
+                        <div className={`list_time ${ingStep === 'time' ? 'block' : 'hidden'}`}>
+                            {
+                                mvTimeList.map((time, kk) => (
+                                    <p key={kk}
+                                        className={`timeEl ${timeEl === time ? 'elSelect' : ''}`}
+                                        onClick={() => { userTimeSelect(time, kk) }}
+                                    >
+                                        {time}
+                                    </p>
+                                ))
+                            }
                         </div>
                     </div>
 
-                    <div className='btn mob'>선택 완료</div>
+                    <div className="btn mob on"
+                        onClick={selectOK}
+                    >선택 완료</div>
                 </div>
             </article>
         </>
