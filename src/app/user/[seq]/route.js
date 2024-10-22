60ed4cb071514be771550786d8bb10c4caece37f
kimziyeon <aa40254037@gmail.com>
2024-10-22 20:42:57 +0900

    20241022 계정 변경,삭제 컴포넌트 추가

--- src/app/user/[seq]/route.js
+++ src/app/user/[seq]/route.js
@@ -1,9 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server';
-import {queryExecute} from '../../../comp/lib/db';
+import { queryExecute } from '../../../comp/lib/db';
 
 
 
-export async function DELETE(req,{params}){
-    const data = await queryExecute('delete from user where idx=?',[params.seq]);
+export async function DELETE(req, { params }) {
+    // console.log(params.seq, 'params.seq')
+    const data = await queryExecute('delete from user where idx=?', [params.seq]);
     return NextResponse.json(data);
 }
---
+++ src/comp/UIUX/Change.jsx
@@ -0,0 +1,20 @@
+
+'use client';
+import React, { useEffect, useRef, useState } from 'react';
+
+function Change(props) {
+    return (
+        <div className='change_pw'>
+            <span className='close'>close_btn</span>
+            <h2>비밀번호 변경</h2>
+            <p>이메일을 정확히 입력해주세요.</p>
+            <div className='input_space'>
+                <input type="text" placeholder='이메일 (abc@mail.com)' />
+            </div>
+            <button className='btn' >아이디 찾기</button>
+            <p className='find_pw_btn'>비밀번호 찾기 {'>'}</p>
+        </div>
+    );
+}
+
+export default Change;
