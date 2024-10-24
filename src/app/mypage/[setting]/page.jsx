606ebd0b971722a1a8a82eb076f53f35833a6ba1
kimziyeon <aa40254037@gmail.com>
2024-10-24 19:56:29 +0900

    20241024 계정설정

--- src/app/mypage/[setting]/page.jsx
+++ src/app/mypage/[setting]/page.jsx
@@ -1,16 +1,73 @@
-import React from 'react';
+"use client"
+import { useState } from 'react';
+import { useSearchParams } from "next/navigation";
 import '../../../comp/style/change.scss';
-function Page(props) {
+
+function Page() {
+
+    const params = useSearchParams();
+    const type = params.get('type');
+    // console.log(type, 'type')
+
+
     return (
-        <div className='change_pw'>
-            <span className='close'>close_btn</span>
-            <h2>비밀번호 변경</h2>
-            <p>이메일을 정확히 입력해주세요.</p>
-            <div className='input_space'>
-                <input type="text" placeholder='이메일 (abc@mail.com)' />
-            </div>
-            <button className='btn' >아이디 찾기</button>
-            <p className='find_pw_btn'>비밀번호 찾기 {'>'}</p>
+
+        <div className='setting_pop'>
+
+            {
+                type === 'change' && (
+                    <div className='user_setting'>
+                        <span className='close'>close_btn</span>
+                        <h2>비밀번호 변경</h2>
+
+
+                        <div className='comp'>
+                            <p>현재 사용중인<br /> 비밀번호를 입력해주세요.</p>
+                            <div className='input_space'>
+                                <input type="text" placeholder='비밀번호' />
+                            </div>
+                            <button className='btn mob'>확인</button>
+                        </div>
+
+                        <div className='comp'>
+                            <p>변경할 비밀번호를 입력해주세요.</p>
+                            <div className='input_space'>
+                                <input type="text" placeholder='비밀번호' />
+                            </div>
+                            <button className='btn mob'>비밀번호 변경</button>
+                        </div>
+
+                    </div>
+
+                )
+            }
+
+            {
+                type === 'delete' && (
+
+                    <div className='user_setting'>
+                        <span className='close'>close_btn</span>
+                        <h2>계정 삭제</h2>
+
+                        <div className='comp'>
+                            <p>안전한 계정 삭제를 위해<br />
+                                현재 사용중인 비밀번호를 입력해주세요.
+                            </p>
+                            <div className='input_space'>
+                                <input type="text" placeholder='비밀번호' />
+                            </div>
+                            <button className='btn mob del'>계정 삭제</button>
+                        </div>
+
+                    </div>
+
+                )
+            }
+
+
+
+
+
         </div>
     );
 }
--- src/comp/UIUX/Setting.jsx
+++ src/comp/UIUX/Setting.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+'use client';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation'
 import '../style/setting.scss'
 import Link from 'next/link';
 import Image from 'next/image';
 
-function Setting({ setSettingClick }) {
+function Setting({ setSettingClick, }) {
+
 
     return (
         <div className='setting_page'>
@@ -11,8 +14,20 @@ function Setting({ setSettingClick }) {
                 <Image src='/images/arrow_back.png' width={30} height={30} alt='arrow_back_icon' onClick={() => { setSettingClick(false) }} />
                 계정 설정
             </h2>
-            <Link href='/mypage/setting'><button className='btn'>비밀번호 변경</button></Link>
-            <Link href='#'><button className='btn'>계정 삭제</button></Link>
+
+            <Link href={{
+                pathname: '/mypage/setting',
+                query: { type: 'change' }
+            }}>
+                <button className='btn' >비밀번호 변경</button>
+            </Link>
+
+            <Link href={{
+                pathname: '/mypage/setting',
+                query: { type: 'delete' }
+            }}>
+                <button className='btn'>계정 삭제</button>
+            </Link>
         </div>
     );
 }
